9ac1443a97f9eb2e9fc8e45e85496a3ab931663c
maks <mkozaev@mail.ru>
2021-11-17 19:14:10 +0300

    refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,13 +18,12 @@ class User < ApplicationRecord
 
   before_save :encrypt_password
 
-  def encrypt_password
-    if self.password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
-      )
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
     end
   end
 
@@ -32,12 +31,15 @@ class User < ApplicationRecord
     password_hash.unpack('H*')[0]
   end
 
-  def self.authenticate(email, password)
-    user = find_by(email: email)
-    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
-      user
-    else
-      nil
+  private
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
     end
   end
 end
