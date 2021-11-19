9a9db508940a9c525f0d62fd1fa2f3c81b2a2213
maks <mkozaev@mail.ru>
2021-11-19 23:21:01 +0300

    user + sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,60 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: %i[index create new]
+
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Maks',
-        username: 'Raziel',
-        avatar_url: 'https://www.gravatar.com/avatar/7cb46e42cebe800660d429e8fa265154?s=100'
-      ),
-      User.new(
-        id: 2,
-        name: 'Irina',
-        username: 'basinka'
-      )
-    ]
+    @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены!'
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @user = User.new(
-      name: 'Maksim',
-      username: 'Aspidius',
-      avatar_url: 'https://www.gravatar.com/avatar/7cb46e42cebe800660d429e8fa265154?s=100'
-    )
-
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('19.11.2021')),
-      Question.new(text: 'В чем смысл жизни?', answer: 'Фиг его', created_at: Date.parse('19.11.2021')),
-      Question.new(text: 'Еще вопрос', created_at: Date.parse('19.11.2021')),
-      Question.new(text: 'И еще вопрос', created_at: Date.parse('19.11.2021'))
-    ]
-
-    @new_question = Question.new
+    @questions = @user.questions.order(created_at: :desc)
+    @new_question = @user.questions.build
     @questions_count = @questions.count
     @answers_count = @questions.count(&:answer)
     @unanswered_count = @questions_count - @answers_count
+  end
+
+  private
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
   end
 end
